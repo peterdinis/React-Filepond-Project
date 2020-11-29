cec5df314566a7e1abe14429c7c8de5ea048c673
Peter Dinis <peterdinis0@pobox.sk>
2020-11-29 19:31:17 +0100

    Fix problem with download images

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import React, {Component} from 'react'
 import './App.css';
 import { FilePond, registerPlugin } from 'react-filepond';
-
-// Import FilePond styles
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginGetFile from 'filepond-plugin-get-file';
-import './filepond-plugin-get-file/dist/filepond-plugin-get-file.min.css';
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
+import 'filepond-plugin-get-file/dist/filepond-plugin-get-file.min.css';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-
-// Register the plugins
 registerPlugin(FilePondPluginImagePreview, FilePondPluginGetFile, FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
-
 
 class App extends Component {
   constructor(props) {
       super(props);
 
       this.state = {
-          // Set initial files, type 'local' means this is a file
-          // that has already been uploaded to the server (see docs)
-          files: [{
+        /*   files: [{
               source: 'index.html',
               options: {
                   type: 'local'
               }
-          }]
+          }] */
       };
   }
   
@@ -43,7 +33,9 @@ class App extends Component {
           <div className="App">
               <h1>React Upload Files with Filepond</h1>
               {/* Pass FilePond properties as attributes */}
-              <FilePond ref={ref => this.pond = ref}
+              <FilePond 
+                        className='filepond'
+                        ref={ref => this.pond = ref}
                         files={this.state.files}
                         allowMultiple={true}
                         allowDownloadByUrl={false}
